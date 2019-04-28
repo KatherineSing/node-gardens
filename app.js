11a160230f2f549c53b6e9eb4adf316588e850c2
katherinesing <ksing@usc.edu>
2019-04-27 21:05:35 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,39 @@
+let express = require('express'); //connects to express routing lib
+let knex = require('knex'); //database
+let app = express();
+app.get('/api/gardens', function(request, response){
+	let connection = knex({
+		client: 'sqlite3',
+		connection: {
+			filename: 'database.sqlite'
+		}
+	});
+	connection.select().from('gardens').then((gardens) => {
+		response.json([gardens]);
+	});
+});
+
+app.get('/api/gardens/:id', function(request, response){
+	let id = request.params.id;
+	let connection = knex({
+		client: 'sqlite3',
+		connection: {
+			filename: 'database.sqlite'
+		}
+	});
+	connection
+	.select().from('gardens')
+	.where('garden_id', id)
+	.first()
+	.then((garden) => {
+		if (garden){
+			response.json([garden]);
+		} 
+		else {
+			response.status(404).json({
+				error: `Garden ${id} not found`
+			});
+		}
+	});
+});
+app.listen(8000);
