085308cb6a0176666ac8dbe8c578df5fcb53114d
katherinesing <ksing@usc.edu>
2019-04-29 10:04:36 -0700

    tests for get, post

---
+++ __tests__/api/crop-test.js
@@ -0,0 +1,50 @@
+const frisby = require('frisby');
+
+const Joi = frisby.Joi;
+
+it('should return a status of 200 when the crop is found', () => {
+	return frisby
+		.get('http://localhost:8000/api/crops/5')
+		.expect('status', 200)
+});
+
+it('should return a status of 404 when the crop does not exist', () => {
+	return frisby
+		.get('http://localhost:8000/api/crops/-1')
+		.expect('status', 404)
+});
+
+it('should return the crop garden_id, title, status, date, description, and its garden', () => {
+	return frisby
+		.get('http://localhost:8000/api/crops/5')
+		.expect('json', 'garden_id', 1)
+		.expect('json', 'title', 'Tomato')
+		.expect('json', 'status', 'Just Planted')
+		.expect('json', 'date', '2019/04/26')
+		.expect('json', 'description', 'Cherry tomatoes planted in the SW corner')
+		.expect('jsonTypes', 'garden', {
+			id: Joi.number().required(),
+			name: Joi.string().required(),
+			address: Joi.string().required()
+		})
+
+});
+
+it('should create a crop', () => {
+	return frisby
+		.post('http://localhost:8000/api/crops', {
+			garden_id: 1,
+			title: "API test apple",
+			status: "Just Planted",
+			date: "2019/04/28",
+			description: "honeycrisp"
+
+		})
+		.expect('status', 200)
+		.expect('json', 'garden_id', 1)
+		.expect('json', 'title', 'API test apple')
+		.expect('json', 'status', 'Just Planted')
+		.expect('json', 'date', '2019/04/28')
+		.expect('json', 'description', 'honeycrisp')
+		.expect('jsonTypes', 'id', Joi.number().required());
+});
---
+++ __tests__/api/garden-test.js
@@ -0,0 +1,43 @@
+const frisby = require('frisby');
+
+const Joi = frisby.Joi;
+
+it('should return a status of 200 when the garden is found', () => {
+	return frisby
+		.get('http://localhost:8000/api/gardens/2')
+		.expect('status', 200)
+});
+
+it('should return a status of 404 when the garden does not exist', () => {
+	return frisby
+		.get('http://localhost:8000/api/gardens/-1')
+		.expect('status', 404)
+});
+
+it('should return the crop garden-id, title, status, date, description, and its garden', () => {
+	return frisby
+		.get('http://localhost:8000/api/gardens/2')
+		.expect('json', 'name', 'Good Earth Community Garden')
+		.expect('json', 'address', '5546-5598 Boden St, Los Angeles, CA 90016')
+		.expect('jsonTypes', 'crops.*', {
+			id: Joi.number().required(),
+			garden_id: Joi.number().required(),
+			title: Joi.string().required(),
+			status: Joi.string().required(),
+			date: Joi.string().required(),
+			description: Joi.string().required()
+		})
+});
+
+it('should create a garden', () => {
+	return frisby
+		.post('http://localhost:8000/api/gardens', {
+			name: "API test garden",
+			address: "123 Street Rd"
+
+		})
+		.expect('status', 200)
+		.expect('json', 'name', 'API test garden')
+		.expect('json', 'address', '123 Street Rd')
+		.expect('jsonTypes', 'id', Joi.number().required());
+});
